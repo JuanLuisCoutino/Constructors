a516a9a242dacfa7b219e4f11caadd10c24b37ab
JuanLuisCoutino <introspectivemotion@gmail.com>
2023-03-22 19:46:29 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+
+    var persona = Persona("Juan Luis", 19)
+    println("Tu nombre es ${persona.nombre} y tu edad es ${persona.edad}")
+
+}
---
+++ src/main/kotlin/Persona.kt
@@ -0,0 +1,6 @@
+class Persona(_nombre : String, _edad : Int) {
+    var nombre : String = _nombre
+    var edad : Int = _edad
+
+
+}
