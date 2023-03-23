353e4e1d0ce2dd486f2950b2d09a44f5447e4129
JuanLuisCoutino <123596662+JuanLuisCoutino@users.noreply.github.com>
2023-03-22 19:48:10 -0600

    Update Persona.kt

--- src/main/kotlin/Persona.kt
+++ src/main/kotlin/Persona.kt
@@ -2,5 +2,11 @@ class Persona(_nombre : String, _edad : Int) {
     var nombre : String = _nombre
     var edad : Int = _edad
 
+    /*
+    constructor(_nombre : String, _edad: Int){
+        nombre = _nombre
+        edad = _edad
+    }
+    */
 
 }
